7714a173add7252ff8b302e3b7a4932990590ac7
tdengler <tomdengler@gmail.com>
2014-09-19 13:02:35 -0400

    make the function comments a bit clearer and add some whitespace

--- cachematrix.R
+++ cachematrix.R
@@ -23,16 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## caches the inverse of a matrix such that it can does not
-## have to be re-calculated each time
+## allows retrieval of a cached matrix inverse calculation
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
+
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
   data <- x$get()
   m <- solve(data, ...)
   x$setinv(m)
